2651ec193eb277c933f91b3a8ae5f65f68bdcca5
kwon5700 <kwon777071@gmail.com>
2025-05-13 01:58:06 +0900

    feat: 임시 cors 설정

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from app.schemas.refine_word import RequestType
 from app.services.refine_word import refine_word
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
     
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/")
 async def vector(request: RequestType):
